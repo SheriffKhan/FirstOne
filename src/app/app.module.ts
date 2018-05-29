01a419d3695ab514dafd6da162f5649d007fb264
Sharif Khan <sharif.khan@irissoftware.com>
2018-05-29 09:21:12 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,11 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { MatToolbarModule } from '@angular/material';
 import { AppComponent } from './app.component';
 
-
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports: [MatToolbarModule],
+  exports: [MatToolbarModule],
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
+
